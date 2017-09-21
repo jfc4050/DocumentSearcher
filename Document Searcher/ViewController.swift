6ba41bf7ebdc650339af07940d2c6ae5c560e307
jfc4050 <jfchiu@uw.edu>
2017-09-20 20:08:28 -0700

    Initial Commit

---
+++ Document Searcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Document Searcher
+//
+//  Created by Justin Chiu on 9/20/17.
+//  Copyright © 2017 Justin Chiu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
